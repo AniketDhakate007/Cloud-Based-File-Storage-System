a439bdc9152d23efeada8347c4720be0e3698cfb
AniketDhakate007 <adhakate344@gmail.com>
2025-11-07 23:55:31 +0530

    Initial commit

---
+++ FileStorageApplication/src/main/java/com/Task2/FileStorageApplication/FileStorageApplication.java
@@ -0,0 +1,13 @@
+package com.Task2.FileStorageApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStorageApplication.class, args);
+	}
+
+}
